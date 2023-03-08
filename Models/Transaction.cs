189cac9a84504a156914830fbb9c2c1df75101ef
Luka Belic <llukaa99@gmail.com>
2023-03-08 09:43:11 +0100

    first

---
+++ Models/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Consumption.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId  { get; set; }
+
+        [Range(1,int.MaxValue,ErrorMessage ="Please select category")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Amount sould be greater than 0")]
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon { get { return Category == null ? "" : Category.Icon + " " + Category.Title; } }
+        [NotMapped]
+        public string? FormatedAmount { get { return ((Category == null || Category.Type == "Expense") ? "-" : "+") + Amount.ToString("C0"); } }
+    }
+}
